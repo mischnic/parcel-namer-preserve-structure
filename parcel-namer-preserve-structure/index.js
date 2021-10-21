4f56c9254e2f2fae2efac74b3a132183087ea58d
Niklas Mischkulnig <4586894+mischnic@users.noreply.github.com>
2021-10-21 18:07:40 +0200

    Update for Parcel stable
    Closes #13

--- parcel-namer-preserve-structure/index.js
+++ parcel-namer-preserve-structure/index.js
@@ -1,27 +1,25 @@
 const { Namer } = require("@parcel/plugin");
-const ThrowableDiagnostic = require("@parcel/diagnostic");
+const { default: ThrowableDiagnostic, md } = require("@parcel/diagnostic");
 const assert = require("assert");
 const path = require("path");
 const nullthrows = require("nullthrows");
 
 const COMMON_NAMES = new Set(["index", "src", "lib"]);
+const ALLOWED_EXTENSIONS = {
+  js: ["js", "mjs", "cjs"],
+};
 
-module.exports = (new Namer({
-  name({ bundle, bundleGraph, options }) {
-    // If the bundle has an explicit file path given (e.g. by a target), use that.
-    if (bundle.filePath != null) {
-      // TODO: what about multiple assets in the same dep?
-      // e.g. input is a Vue file, output is JS + CSS
-      // which is defined as a target in package.json?
-      return bundle.filePath;
-    }
-
+module.exports = new Namer({
+  name({ bundle, bundleGraph }) {
     let bundleGroup = bundleGraph.getBundleGroupsContainingBundle(bundle)[0];
-    let bundleGroupBundles = bundleGraph.getBundlesInBundleGroup(bundleGroup);
+    let bundleGroupBundles = bundleGraph.getBundlesInBundleGroup(bundleGroup, {
+      includeInline: true,
+    });
+    let isEntry = bundleGraph.isEntryBundleGroup(bundleGroup);
 
-    if (bundle.isEntry) {
+    if (bundle.needsStableName) {
       let entryBundlesOfType = bundleGroupBundles.filter(
-        (b) => b.isEntry && b.type === bundle.type
+        (b) => b.needsStableName && b.type === bundle.type
       );
       assert(
         entryBundlesOfType.length === 1,
@@ -38,37 +36,38 @@ module.exports = (new Namer({
 
     if (
       bundle.id === mainBundle.id &&
-      bundle.isEntry &&
+      isEntry &&
       bundle.target &&
       bundle.target.distEntry != null
     ) {
       let loc = bundle.target.loc;
       let distEntry = bundle.target.distEntry;
-      if (
-        path.extname(bundle.target.distEntry).slice(1) !== bundle.type &&
-        loc
-      ) {
+      let distExtension = path.extname(bundle.target.distEntry).slice(1);
+      let allowedExtensions = ALLOWED_EXTENSIONS[bundle.type] || [bundle.type];
+      if (!allowedExtensions.includes(distExtension) && loc) {
         let fullName = path.relative(
           path.dirname(loc.filePath),
           path.join(bundle.target.distDir, distEntry)
         );
         let err = new ThrowableDiagnostic({
           diagnostic: {
-            message: `Target "${bundle.target.name}" declares an output file path of "${fullName}" which does not match the compiled bundle type "${bundle.type}".`,
-            filePath: loc.filePath,
-            codeFrame: {
-              codeHighlights: [
-                {
-                  start: loc.start,
-                  end: loc.end,
-                  message: `Did you mean "${
-                    fullName.slice(0, -path.extname(fullName).length) +
-                    "." +
-                    bundle.type
-                  }"?`,
-                },
-              ],
-            },
+            message: md`Target "${bundle.target.name}" declares an output file path of "${fullName}" which does not match the compiled bundle type "${bundle.type}".`,
+            codeFrames: [
+              {
+                filePath: loc.filePath,
+                codeHighlights: [
+                  {
+                    start: loc.start,
+                    end: loc.end,
+                    message: md`Did you mean "${
+                      fullName.slice(0, -path.extname(fullName).length) +
+                      "." +
+                      bundle.type
+                    }"?`,
+                  },
+                ],
+              },
+            ],
             hints: [
               `Try changing the file extension of "${
                 bundle.target.name
@@ -87,43 +86,43 @@ module.exports = (new Namer({
     //      `index.css`.
     let name = nameFromContent(
       mainBundle,
+      isEntry,
       bundleGroup.entryAssetId,
-      options.entryRoot
+      bundleGraph.getEntryRoot(bundle.target)
     );
-    if (!bundle.isEntry) {
+    if (!bundle.needsStableName) {
       name += "." + bundle.hashReference;
     }
 
     return name + "." + bundle.type;
   },
-}) /*: Namer*/);
+});
 
-function nameFromContent(
-  bundle /*: Bundle*/,
-  entryAssetId /*: string*/,
-  entryRoot /*: FilePath*/
-) /*: string*/ {
+function nameFromContent(bundle, isEntry, entryAssetId, entryRoot) {
   let entryFilePath = nullthrows(
     bundle.getEntryAssets().find((a) => a.id === entryAssetId)
   ).filePath;
   let name = basenameWithoutExtension(entryFilePath);
 
   // If this is an entry bundle, use the original relative path.
-  // if (bundle.isEntry) {
+  // if (bundle.needsStableName) {
   // Match name of target entry if possible, but with a different extension.
-  if (bundle.target.distEntry != null) {
+  if (isEntry && bundle.target.distEntry != null) {
     return basenameWithoutExtension(bundle.target.distEntry);
   }
 
   return path
     .join(path.relative(entryRoot, path.dirname(entryFilePath)), name)
-    .replace(/\.\.(\/|\\)/g, "__$1");
+    .replace(/\.\.(\/|\\)/g, "up_$1");
   // } else {
   //   // If this is an index file or common directory name, use the parent
   //   // directory name instead, which is probably more descriptive.
   //   while (COMMON_NAMES.has(name)) {
   //     entryFilePath = path.dirname(entryFilePath);
   //     name = path.basename(entryFilePath);
+  //     if (name.startsWith('.')) {
+  //       name = name.replace('.', '');
+  //     }
   //   }
 
   //   return name;
